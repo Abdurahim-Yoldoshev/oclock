9c2c98fb55c078ed8e17e3fbec0cb8745ed95379
Abdurahim-Yoldoshev <yoldoshevcoder@gmail.com>
2025-02-24 21:07:07 +0500

    Add JavaScript functionality to update clock display

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+
+function upDateClock() {
+    const now = new Date();
+    if (now.getHours < 10) {
+        hours.innerHTML = `0${now.getHours().toString()}`;
+    } else {
+        hours.innerHTML = now.getHours().toString();
+    };
+
+    if (now.getMinutes < 10) {
+        minutes.innerHTML = `0${now.getMinutes().toString()}`;
+    } else {
+        minutes.innerHTML = now.getMinutes().toString();
+    };
+}
+
+setInterval(upDateClock,1000);
+upDateClock();
